49b8752bd00228ed09dd0f7d8f54e21d8374096c
Ayush Saini <005ayushsaini@gmail.com>
2025-02-13 14:22:33 +0530

    Changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,8 @@
-export const USER_API_ENDPOINT = "http://localhost:8080/api/v1/user";
-export const JOB_API_ENDPOINT = "http://localhost:8080/api/v1/jobs";
+export const USER_API_ENDPOINT =
+  "https://job-portal-api-dt5y.onrender.com/api/v1/user";
+export const JOB_API_ENDPOINT =
+  "https://job-portal-api-dt5y.onrender.com/api/v1/jobs";
 export const APPLICATION_API_ENDPOINT =
-  "http://localhost:8080/api/v1/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:8080/api/v1/company";
+  "https://job-portal-api-dt5y.onrender.com/api/v1/application";
+export const COMPANY_API_ENDPOINT =
+  "https://job-portal-api-dt5y.onrender.com/api/v1/company";

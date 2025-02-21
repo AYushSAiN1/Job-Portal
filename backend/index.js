bb564eb158fc94ca74d88046661e65e491e6b2a2
Ayush Saini <005ayushsaini@gmail.com>
2025-02-21 17:44:45 +0530

    Changed URL

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
   })
 );
 
